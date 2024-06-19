2fd1bb74916739fc2af69373348fa40fc219c1b5
Vitaly Matseiko <realnatisk@gmail.com>
2024-06-19 18:11:11 +0200

    admin dashboard display dayly stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,6 +6,28 @@ class AdminController < ApplicationController
   layout 'admin'
 
   def index
-    @post = []
+    today_start = Time.zone.now.midnight
+    today_end = today_start.end_of_day
+
+    query = <<-SQL
+      SELECT
+        COUNT(DISTINCT orders.id) AS orders_count,
+        COUNT(DISTINCT order_products.id) AS order_products_count,
+        SUM(total) AS total_sum,
+        AVG(total) AS total_average
+      FROM orders
+      LEFT JOIN order_products ON orders.id = order_products.order_id
+      WHERE orders.created_at >= '#{today_start}' AND orders.created_at <= '#{today_end}'
+    SQL
+
+    todays_orders = ActiveRecord::Base.connection.execute(query).first
+
+    @dashboard_data = {
+      revenue: todays_orders['total_sum'],
+      orders_count: todays_orders['orders_count'],
+      avg_sale: todays_orders['total_average'],
+      per_sale: todays_orders['order_products_count']
+    }
+    end
   end
 end
