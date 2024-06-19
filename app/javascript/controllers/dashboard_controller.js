547914f867eca47ab8fbd6d52a4cab1e7d1f63db
Vitaly Matseiko <realnatisk@gmail.com>
2024-06-19 22:59:44 +0200

    remove vendor scripts for chart js and kurkle and use remote links + use chart with fake data

---
+++ app/javascript/controllers/dashboard_controller.js
@@ -0,0 +1,53 @@
+import { Controller } from "@hotwired/stimulus"
+import { Chart, registerables } from "chart.js"
+
+Chart.register(...registerables)
+
+// Connects to data-controller="dashboard"
+export default class extends Controller {
+  // connect() {
+  //   console.log("Hello hallo pryvit");
+  // };
+
+  initialize() {
+    const data = [10, 20, 30, 40, 50, 60, 70];
+    const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    const container = document.getElementById("revenueChart");
+    
+    new Chart(container, {
+      type: "line",
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Revenue $",
+          data: data,
+          borderWidth: 1,
+          fill: true
+        }]
+      },
+      optioins: {
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            border: {
+              dash: [5, 5]
+            },
+            grid: {
+              color: "#dedede"
+            },
+            beginAtZero: true
+          }
+        }
+      }
+    })
+  }
+}
