a5dec924910ebd3f642e35d88226047983374167
Vitaly Matseiko <realnatisk@gmail.com>
2024-06-21 22:04:22 +0200

    dynamic data to chart disaply on admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,8 +6,8 @@ class AdminController < ApplicationController
   layout 'admin'
 
   def index
-    today_start = Time.zone.now.midnight
-    today_end = today_start.end_of_day
+    today_start = Time.zone.now.midnight - 7.days
+    today_end = Time.zone.now.midnight.end_of_day
 
     query = <<-SQL
       SELECT
@@ -17,16 +17,29 @@ class AdminController < ApplicationController
         AVG(total) AS total_average
       FROM orders
       LEFT JOIN order_products ON orders.id = order_products.order_id
-      WHERE orders.created_at >= '#{today_start}' AND orders.created_at <= '#{today_end}'
+      WHERE orders.created_at >= '#{today_start}' AND orders.created_at <= '#{today_end}';
     SQL
 
-    todays_orders = ActiveRecord::Base.connection.execute(query).first
+    todays_orders = ActiveRecord::Base.connection.execute(query)
 
     @dashboard_data = {
-      revenue: todays_orders['total_sum'],
-      orders_count: todays_orders['orders_count'],
-      avg_sale: todays_orders['total_average'],
-      per_sale: todays_orders['order_products_count']
+      revenue: todays_orders.first['total_sum'],
+      orders_count: todays_orders.first['orders_count'],
+      avg_sale: todays_orders.first['total_average'],
+      per_sale: todays_orders.first['order_products_count']
     }
+
+    day_names = Date::DAYNAMES
+    initial_stat_hash = day_names.map { |day| [day, 0] }.to_h # { Sunday: 0, Monday: 0, Tuesday: 0 ....}
+
+    orders_by_day = Order
+      .select("EXTRACT(DOW FROM created_at) AS day_of_week, SUM(total) AS total_sum")
+      .where('created_at > ?', Time.now.midnight - 7.days)
+      .group("day_of_week")
+      .order("day_of_week")
+
+    orders_by_day = orders_by_day.map { |order| [day_names[order.day_of_week.to_i], order.total_sum.to_f] }.to_h
+
+    @revenue_by_day = initial_stat_hash.merge!(orders_by_day).to_a
   end
 end
--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -5,12 +5,14 @@ Chart.register(...registerables)
 
 // Connects to data-controller="dashboard"
 export default class extends Controller {
+  static values = { revenue: Array }
+
   initialize() {
-    const data = [10, 20, 30, 40, 50, 60, 70];
-    const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    const container = document.getElementById("revenueChart");
+    const data = this.revenueValue.map((item) => item[1]/100.0) //;
+    const labels = this.revenueValue.map((item) => item[0]);
+    const ctx = document.getElementById("revenueChart");
     
-    new Chart(container, {
+    new Chart(ctx, {
       type: "line",
       data: {
         labels: labels,
