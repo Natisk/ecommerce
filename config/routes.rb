6daa4701cd7f15590f0d2a19da697a739dc31e8b
Vitaly Matseiko <realnatisk@gmail.com>
2024-07-03 19:24:41 +0200

    add backend route and ctrl for checkout

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   resources :categories, only: %i[index show]
   resources :products, only: %i[index show]
 
+  post 'checkout' => 'checkouts/create'
   get 'cart' => 'carts#show'
 
   # Route to handle external reference in chart.js
