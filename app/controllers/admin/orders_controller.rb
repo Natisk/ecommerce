14005bb5fba82f092a7b7dc04199ea01b44c4da6
Vitaly Matseiko <realnatisk@gmail.com>
2024-07-19 11:13:05 +0200

    latest first for admin orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,7 +5,7 @@ class Admin::OrdersController < AdminController
 
   # GET /orders or /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.order(created_at: :desc)
   end
 
   # GET /orders/1 or /orders/1.json
@@ -25,7 +25,7 @@ class Admin::OrdersController < AdminController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to admin_order_url(@order), notice: "Order was successfully created." }
+        format.html { redirect_to admin_order_url(@order), notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class Admin::OrdersController < AdminController
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to admin_order_url(@order), notice: "Order was successfully updated." }
+        format.html { redirect_to admin_order_url(@order), notice: 'Order was successfully updated.' }
         format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +52,20 @@ class Admin::OrdersController < AdminController
     @order.destroy!
 
     respond_to do |format|
-      format.html { redirect_to admin_orders_url, notice: "Order was successfully destroyed." }
+      format.html { redirect_to admin_orders_url, notice: 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:customer_email, :fulfilled, :total, :address)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:customer_email, :fulfilled, :total, :address)
+  end
 end
